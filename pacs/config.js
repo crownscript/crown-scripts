36c98d839afd11f0a615df1dcf108d0b322e738f
crownscript <info@crown-outsourcing.com>
2025-03-16 18:52:07 -0400

    Update config.js

--- pacs/config.js
+++ pacs/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    seasonalMessage: "Join our yearly drive ",  // Change this as needed
+    seasonalMessage: "YEARLY DRIVE",  // Change this as needed
 };
 
 // Function to get the seasonal message dynamically
