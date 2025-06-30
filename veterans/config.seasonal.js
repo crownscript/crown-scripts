9250e867e80cb521ad2e51dcb94a8a077cd5dc4c
paulpccu <paulp.ccu@gmail.com>
2025-06-30 17:04:42 -0400

    ADDED CPOD AND VAA AND FOLDERS FOR VETS AND FIREFIGHTERS

---
+++ veterans/config.seasonal.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    seasonalMessage: "INDEPENDENCE DAY DRIVE",  // Change this as needed
+};
+
+// Function to get the seasonal message dynamically
+function getSeasonalMessage() {
+    return CONFIG.seasonalMessage;
+}
