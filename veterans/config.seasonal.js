6308d0f864b8d9102dc7b486139a706e2a62a6f5
paulpccu <paulp.ccu@gmail.com>
2026-04-27 13:02:42 -0400

    update  seasonal message spelling

--- veterans/config.seasonal.js
+++ veterans/config.seasonal.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    seasonalMessage: "MEMORIALDAY DRIVE",  // Change this as needed
+    seasonalMessage: "MEMORIAL DAY DRIVE",  // Change this as needed
 };
 
 // Function to get the seasonal message dynamically
